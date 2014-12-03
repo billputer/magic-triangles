d75b6fce2b40c3a6f673cb81261cc1f7a864ea46
Bill Wiens <billwiens@gmail.com>
2014-12-03 14:38:38 -0800

    Separate triangle creation and adding to stage.

--- triangles.js
+++ triangles.js
@@ -2,9 +2,9 @@ $(document).ready(function init() {
   var canvas = document.getElementById('easel'),
   stage = new createjs.Stage(canvas),
   initial_triangle_size = 400,
-  t2 = [];
+  t2, t3, t4, t5;
   
-  function drawTriangle(stage, size, x, y) {
+  function createTriangle(stage, size, x, y) {
     var shape = new createjs.Shape(),
     g = shape.graphics,
     horizontal_offset = size / 2,
@@ -21,23 +21,26 @@ $(document).ready(function init() {
     
     shape.x = x;
     shape.y = y;
-    
-    stage.addChild(shape);
-    stage.update();
   
     return shape;
   }
   
-  drawTriangle(stage, initial_triangle_size, 250, 250);
-  t1 = drawTriangle(stage, initial_triangle_size, 250, 250);
+  // t1 setup
+  stage.addChild(createTriangle(stage, initial_triangle_size, 250, 250));
+  t1 = createTriangle(stage, initial_triangle_size, 250, 250);
+  stage.addChild(t1);
   
+  // t2 setup
+  t2 = [];
+  t2.push(createTriangle(stage, initial_triangle_size / 3, 250, 96));
+
   function t1_rotate() {
-     createjs.Tween.get(t1).to({rotation: 60}, 500)
-      .wait(500)
+    createjs.Tween.get(t1).to({rotation: 60}, 500)
+    .wait(500)
       .call(t2_rotate);
   }
   function t2_rotate() {
-    t2.push(drawTriangle(stage, initial_triangle_size / 3, 250, 96));
+    stage.addChild(t2[0]);
     createjs.Tween.get(t2[0])
       .to({rotation: 60}, 500)
       .wait(500)
@@ -50,7 +53,6 @@ $(document).ready(function init() {
   }
   function t1_reset() {
     stage.removeChild(t2[0]);
-    t2.splice(0, 1);
     createjs.Tween.get(t1)
       .wait(500)
       .to({rotation: 0}, 500)
@@ -62,9 +64,6 @@ $(document).ready(function init() {
   // start tweening
   t1_rotate();
 
-  
-  
-  
   createjs.Ticker.setFPS(60);
   createjs.Ticker.addEventListener("tick", stage);
 
